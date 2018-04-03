ec19220f31ead636b0e457890c3bae9d141e1fa4
Cody Pitts <cpitts@forumone.com>
2018-04-03 10:07:31 -0700

    Test commit

--- cody_app/urls.py
+++ cody_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='home'),
     path('<slug:slug>', views.EventBaseView.as_view(), name='eventbase'),
