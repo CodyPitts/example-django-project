ae2b5b88fd3826cba46595ec9ccdedbdb63e9d4b
Cody Pitts <cpitts@forumone.com>
2018-04-05 15:04:57 -0700

    Fixed URLs, migration experiments

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='home'),
-    path('<slug:slug>', views.EventBaseView.as_view(), name='eventbase'),
-    path('<slug:slug>/speakers', views.SpeakersView.as_view(), name='speakers'),
-    path('<slug:slug>/schedule', views.SessionsView.as_view(), name='schedule'),
+    path('<slug:slug>/', views.EventBaseView.as_view(), name='eventbase'),
+    path('<slug:slug>/speakers/', views.SpeakersView.as_view(), name='speakers'),
+    path('<slug:slug>/schedule/', views.SessionsView.as_view(), name='schedule'),
 ]
