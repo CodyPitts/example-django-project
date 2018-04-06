ad7811ec3970e1fd9024ac53ff3822c4193dd5c4
Cody Pitts <cpitts@forumone.com>
2018-04-05 17:06:50 -0700

    Added default page for startup

--- simple_site/urls.py
+++ simple_site/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simple_app/', include('simple_app.urls')),
     path('simple_api/', include('simple_api.urls')),
+    path('', views.index, name='index'),
 ]
